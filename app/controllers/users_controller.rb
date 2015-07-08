af5ec9389cbbedf2908676fdffb46ac2e8582198
Jessica Engel & ian bevel <student@epicodus.com>
2015-07-08 15:05:02 -0700

    allow user to select and view profile from dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,5 @@
-class UsersController < ActionController::Base
-
-  def index
-  end
-
+class UsersController < ApplicationController
   def show
-
-  end
-
-  def new
-  end
-
-  def create
-    User.create(user_params)
-  end
-
-  def edit
-
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :avatar, :password, :password_confirmation, :remember_me, :username)
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
 
   root to: "home#index"
-  resources :users
 
   get "/log-in" => "sessions#new"
   post "/log-in" => "sessions#create"
   get "/log-out" => "sessions#destroy", as: :log_out
 
+  resources :users, :only => [:show]
 end
